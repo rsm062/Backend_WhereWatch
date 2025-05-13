d93be1fccf2a35e529f51534add059f774bebb5a
Rafael Sánchez <156418688+rsm062@users.noreply.github.com>
2025-05-13 10:34:26 +0200

    Update CountryRepository.java

--- src/main/java/ad/project/backend_wherewhatch/repositories/CountryRepository.java
+++ src/main/java/ad/project/backend_wherewhatch/repositories/CountryRepository.java
@@ -3,5 +3,6 @@ package ad.project.backend_wherewhatch.repositories;
 import ad.project.backend_wherewhatch.models.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 }
